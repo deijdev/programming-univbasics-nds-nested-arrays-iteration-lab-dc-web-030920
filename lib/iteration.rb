51b451bf8b42e385c82788d54a273fa220238054
Deijah Price <deijahwilson1@gmail.com>
2020-02-27 02:11:26 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -32,9 +32,11 @@ array
 end
 
 def total_even_pairs(src)
-   total = 0
+  total = 0
   row_index = 0
+  
   while row_index < src.count do
+    
   if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
   total += src[row_index][0] + src[row_index][1]
   row_index += 1
